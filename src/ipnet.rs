3b9b8a90ce6f0a3f4a0ef04eea8f252d1c81b606
Kris Price <kris@krisprice.nz>
2017-08-16 18:17:26 -0700

    Adding subnets() methods.

--- src/ipnet.rs
+++ src/ipnet.rs
@@ -113,7 +113,28 @@ impl Ipv4Net {
     }
 
     pub fn supernet(&self) -> Ipv4Net {
-        Ipv4Net::new(Ipv4Addr::from(self.octets()), self.prefix_len - 1)
+        Ipv4Net::new(self.addr.clone(), self.prefix_len - 1)
+    }
+
+    // TODO: Should the parameter be depth, as in the number of
+    // divisions to make, or new_prefix_len (self explanatory), or
+    // num_subnets? I suspect in use the later two will be more useful.
+    // TODO: Should this be an iterator?
+    pub fn subnets(&self, new_prefix_len: u8) -> Vec<Ipv4Net> {
+        // TODO: Need to test bounds of new_prefix_len and error.
+        // Or perhaps just set to 32 in that case.
+        // TODO: std::ops::Add missing for Ipv4Addr
+        let rng = 2u32.pow(32 - new_prefix_len as u32);
+        let mut net = u32::from(self.network());
+        let end = u32::from(self.broadcast());
+        let mut res: Vec<Ipv4Net> = Vec::new();
+
+        while net < end {
+            res.push(Ipv4Net::new(Ipv4Addr::from(net), new_prefix_len));
+            net += rng;
+        }
+        res
+
     }
 
     pub fn contains(&self, other: &Ipv4Net) -> bool {
@@ -130,6 +151,10 @@ impl Ipv6Net {
         // TODO: Should error if prefix_len > 128 as prefix_len <= 128
         // is assumed in subsequent methods.
         Ipv6Net { addr: ip, prefix_len: prefix_len, }
+    }
+
+    pub fn addr(&self) -> Ipv6Addr {
+        self.addr
     }
 
     // The u128 type would be nice here, but it's not marked stable yet.
@@ -184,7 +209,39 @@ impl Ipv6Net {
     }
 
     pub fn supernet(&self) -> Ipv6Net {
-        Ipv6Net::new(Ipv6Addr::from(self.segments()), self.prefix_len - 1)
+        Ipv6Net::new(self.addr.clone(), self.prefix_len - 1)
+    }
+
+    // TODO: Is there a nicer way to do this? Using u128 types would be
+    // nice but they're not marked as stable yet. Should this return an
+    // iterator instead of a vector?
+    pub fn subnets(&self, new_prefix_len: u8) -> Vec<Ipv6Net> {
+        // TODO: Need to implement a proper error handling scheme.
+        if new_prefix_len <= self.prefix_len {
+            return Vec::new();
+        }
+
+        let mut network = ipv6_addr_into_double_u64(self.network());
+        let mut broadcast = ipv6_addr_into_double_u64(self.broadcast());
+
+        let mask = ipv6_addr_into_double_u64(Ipv6Net::new(self.addr.clone(), new_prefix_len).hostmask());
+        let mut res: Vec<Ipv6Net> = Vec::new();
+
+        while network < broadcast {
+            res.push(Ipv6Net::new(ipv6_addr_from_double_u64(network), new_prefix_len));
+            
+            network[0] |= mask[0];
+            network[1] |= mask[1];
+
+            let (lo, ov) = network[1].overflowing_add(1);
+            network[1] = lo;
+            
+            if ov {
+                network[0] += 1;
+            }
+        }
+
+        res
     }
 
     pub fn contains(&self, other: &Ipv6Net) -> bool {
@@ -194,6 +251,30 @@ impl Ipv6Net {
     pub fn sibling(&self, other: &Ipv6Net) -> bool {
         self.prefix_len == other.prefix_len && self.supernet().contains(other)
     }
+}
+
+// TODO: It would be nice to implement From on Ipv6Addr for this.
+pub fn ipv6_addr_from_double_u64(segments: [u64; 2]) -> Ipv6Addr {
+    let (hi, lo) = (segments[0], segments[1]);
+    Ipv6Addr::new(
+        (hi >> 48) as u16, (hi >> 32) as u16, (hi >> 16) as u16, hi as u16,
+        (lo >> 48) as u16, (lo >> 32) as u16, (lo >> 16) as u16, lo as u16
+    )
+}
+
+// TODO: It would be nice to implement From on Ipv6Addr for this.
+pub fn ipv6_addr_into_double_u64(ip: Ipv6Addr) -> [u64; 2] {
+    let ip = ip.octets();
+    [
+        ((ip[0] as u64) << 56) + ((ip[1] as u64) << 48) +
+        ((ip[2] as u64) << 40) + ((ip[3] as u64) << 32) +
+        ((ip[4] as u64) << 24) + ((ip[5] as u64) << 16) +
+        ((ip[6] as u64) << 8) + (ip[7] as u64),
+        ((ip[8] as u64) << 56) + ((ip[9] as u64) << 48) +
+        ((ip[10] as u64) << 40) + ((ip[11] as u64) << 32) +
+        ((ip[12] as u64) << 24) + ((ip[13] as u64) << 16) +
+        ((ip[14] as u64) << 8) + (ip[15] as u64), 
+    ]
 }
 
 impl fmt::Display for IpNet {
@@ -214,43 +295,5 @@ impl fmt::Display for Ipv4Net {
 impl fmt::Display for Ipv6Net {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         write!(fmt, "{}/{}", self.addr, self.prefix_len)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
-    use std::option::Option::{Some, None};
-    use std::str::FromStr;
-    use ipnet::{IpNet, Ipv4Net, Ipv6Net};
-    
-    #[test]
-    fn test_ipv4_net() {
-        // TODO: Test the boundaries (/0, /32, /64, /128). Or iterate
-        // over every bit.
-        assert_eq!(
-            IpNet::from_str("10.0.0.0/8").unwrap(),
-            IpNet::V4(Ipv4Net { addr: Ipv4Addr::from_str("10.0.0.0").unwrap(), prefix_len: 8 })
-        );
-        assert_eq!(
-            Ipv4Net::from_str("172.16.0.0/12").unwrap(),
-            Ipv4Net { addr: Ipv4Addr::from_str("172.16.0.0").unwrap(), prefix_len: 12 }
-        );
-        assert_eq!(
-            Ipv4Net::from_str("172.16.0.0/12").unwrap().netmask(),
-            Ipv4Addr::from_str("255.240.0.0").unwrap()
-        );
-        assert_eq!(
-            Ipv4Net::from_str("172.16.0.0/12").unwrap().hostmask(),
-            Ipv4Addr::from_str("0.15.255.255").unwrap()
-        );
-        assert_eq!(
-            Ipv4Net::from_str("172.16.0.0/12").unwrap().network(),
-            Ipv4Addr::from_str("172.16.0.0").unwrap()
-        );
-        assert_eq!(
-            Ipv4Net::from_str("172.16.0.0/12").unwrap().broadcast(),
-            Ipv4Addr::from_str("172.31.255.255").unwrap()
-        );
     }
 }
